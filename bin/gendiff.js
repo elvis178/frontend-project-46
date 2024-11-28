7e876fd080931b5df00d34a3dc3eed13693b3792
elvis178 <elvisss178@gmail.com>
2024-11-28 21:33:42 +0300

    add action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import path from 'path';
+import parseFile from '../src/parser.js'
+
 const program = new Command();
 
 program
@@ -9,5 +12,19 @@ program
   .version('1.0.0')
   .option('-f, --format <type>', 'output format')
   .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    const absolutePath1 = path.resolve(process.cwd(), filepath1);
+    const absolutePath2 = path.resolve(process.cwd(), filepath2);
+
+    const file1Data = parseFile(absolutePath1);
+    const file2Data = parseFile(absolutePath2);
+
+    console.log("Contents of file1.json:");
+    console.log(file1Data);
+    console.log("Contents of file2.json:");
+    console.log(file2Data);
+
+  });
+
   
 program.parse();
