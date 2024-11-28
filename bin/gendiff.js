d7fb89b03cdc030879881280da2f68d133c794c6
elvis178 <elvisss178@gmail.com>
2024-11-28 14:14:01 +0300

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,5 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-h, --help', 'output usage information')
   
   program.parse();
