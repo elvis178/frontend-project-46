86c1b64e27dd1e325a1a166a114b9ecea9c3301a
elvis178 <elvisss178@gmail.com>
2024-12-04 23:56:27 +0300

    update buildDiff.js

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -14,10 +14,10 @@ const genDiff = (data1, data2) => {
       return  `- ${key}: ${data1[key]}`;
     } else if (data1[key] !== data2[key]) {
       // Если значения ключа отличаются
-      return `- ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`;
+      return `- ${key}: ${data1[key]}\n+ ${key}: ${data2[key]}`;
     }
     // Если значения не изменились, то ключ неизменен
-    return `${key}: ${data1[key]}`;
+    return `  ${key}: ${data1[key]}`;
   });
 
   return `{\n${result.join('\n')}\n}`; // Возвращаем результат
