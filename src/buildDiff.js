80a1344d75f1d1cf779a7fa472859a1ea7275d1a
elvis178 <elvisss178@gmail.com>
2024-12-11 20:54:04 +0300

    update buildDiff.js

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -1,26 +1,27 @@
 import _ from 'lodash';
 
 const buildDiff = (data1, data2) => {
-  const keys1 = Object.keys(data1); // Ключи первого объекта
-  const keys2 = Object.keys(data2); // Ключи второго объекта
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
   const keys = _.sortBy(_.union(keys1, keys2)); // Объединяем и сортируем ключи
 
   const result = keys.map((key) => {
     if (!Object.hasOwn(data1, key)) {
-      // Если ключа нет в data1, значит он добавлен в data2
+      // Если ключ отсутствует в data1, значит, он добавлен в data2
       return `+ ${key}: ${data2[key]}`;
     } else if (!Object.hasOwn(data2, key)) {
-      // Если ключа нет в data2, значит он удален из data2
-      return  `- ${key}: ${data1[key]}`;
+      // Если ключ отсутствует в data2, значит, он удалён из data2
+      return `- ${key}: ${data1[key]}`;
     } else if (data1[key] !== data2[key]) {
-      // Если значения ключа отличаются
+      // Если значения ключей отличаются
       return `- ${key}: ${data1[key]}\n+ ${key}: ${data2[key]}`;
     }
-    // Если значения не изменились, то ключ неизменен
+    // Если значения одинаковы, ключ не изменился
     return `  ${key}: ${data1[key]}`;
   });
 
-  return `{\n${result.join('\n')}\n}`; // Возвращаем результат
+  return `{\n${result.join('\n')}\n}`;
 };
+
 
 export default buildDiff;
