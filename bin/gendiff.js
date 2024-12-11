ef8e2cb19bcb9f27f68341a5b58c6e086993250e
elvis178 <elvisss178@gmail.com>
2024-12-11 17:22:21 +0300

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import path from 'path';
-import parseFile from '../src/parser.js';
-import genDiff from '../src/buildDiff.js'
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
@@ -14,16 +12,10 @@ program
   .option('-f, --format <type>', 'output format')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    const absolutePath1 = path.resolve(process.cwd(), filepath1);
-    const absolutePath2 = path.resolve(process.cwd(), filepath2);
-
-    const file1Data = parseFile(absolutePath1);
-    const file2Data = parseFile(absolutePath2);
-
     // Вызов функции genDiff и получение результата
-    const differences = genDiff(file1Data, file2Data);
+    const diff = genDiff(filepath1, filepath2);
     // Вывод результата
-    console.log(differences);
+    console.log(diff);
   }); 
 
 program.parse();
