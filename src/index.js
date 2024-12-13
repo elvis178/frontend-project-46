169b60cbd136a247539584597b6b6261e2388992
elvis178 <elvisss178@gmail.com>
2024-12-13 17:41:34 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -2,20 +2,21 @@ import path from 'path'; //resolve
 import process from 'process'; //cwd
 import parseFile from './parser.js';
 import buildDiff from './buildDiff.js';
+import formatDiff from './formatters/index.js';
 
 
 
 
-const genDiff = (filepath1, filepath2) => {
+const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const absolutePath1 = path.resolve(process.cwd(), filepath1);
   const absolutePath2 = path.resolve(process.cwd(), filepath2);
   
   const file1Data = parseFile(absolutePath1);
   const file2Data = parseFile(absolutePath2);
 
-  const diff = buildDiff(file1Data, file2Data);
+  const diffFree = buildDiff(file1Data, file2Data);
 
-  return diff;
+  return formatDiff(diffFree, format);
 };
 
 export default genDiff;
