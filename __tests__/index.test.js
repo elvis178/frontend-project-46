9ad5ec60911865fcc26ade5e3aa0084985532f69
elvis178 <elvisss178@gmail.com>
2024-12-13 17:38:48 +0300

    update index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,30 +1,26 @@
 import path from 'path';
 import url from 'url';
+import fs from 'fs';
+import { expect, test } from '@jest/globals';
 import genDiff from '../src/index.js';
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
 
 test('genDiff with objects having different keys and values', () => {
   const filepath1 = getFixturePath('file1.json');
   const filepath2 = getFixturePath('file2.json');
+  const expected = readFile('expected-json.txt');
 
-  const expected = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-  }`;
-
-  const result = genDiff(filepath1, filepath2);
+  const result = genDiff(filepath1, filepath2, 'json');
 
   // Логирование для дебага
   console.log(result);
   
   // Сравниваем строки с учётом точного формата
-  expect(result).toBe(expected);
+  expect(result).toEqual(expected);
 });
