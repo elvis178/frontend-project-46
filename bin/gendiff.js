3d45b9350eb519f64a61d7f89a14c4bb739ef153
elvis178 <elvisss178@gmail.com>
2024-12-13 17:39:33 +0300

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,13 +9,11 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => {
-    // Вызов функции genDiff и получение результата
-    const diff = genDiff(filepath1, filepath2);
-    // Вывод результата
+  .action((filepath1, filepath2, options) => {
+    const diff = genDiff(filepath1, filepath2, options.format);
     console.log(diff);
-  }); 
+  });
 
 program.parse();
