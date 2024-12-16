6ba15ecbcdd1a801ecbf415670a163c54b09b25e
elvis178 <elvisss178@gmail.com>
2024-12-16 17:44:53 +0300

    add yml-format

---
+++ src/parsers.js
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import yaml from 'yaml';
+import path from 'path';
+
+const parseFile = (filePath) => {
+  const fileData = fs.readFileSync(filePath, 'utf-8');
+
+  const fileExtension = path.extname(filePath).slice(1).toLowerCase()
+
+  if (fileExtension === 'json') {
+    return JSON.parse(fileData);
+  }
+
+  if (fileExtension === 'yml' || fileExtension === 'yaml') {
+    return yaml.parse(fileData);
+  }
+  throw new Error(`Unsupported file format: ${fileExtension} (ಡ‸ಡ)`);
+};
+
+export default parseFile;
