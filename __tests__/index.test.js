e5f4369a0153d0d6524dea723a93a0cba76a3572
elvis178 <elvisss178@gmail.com>
2024-12-20 20:24:18 +0300

    update

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,26 +10,34 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
+
 test('Json Gendiff, stylish-format', () => {
   const filepath1 = getFixturePath('file1.json');
   const filepath2 = getFixturePath('file2.json');
   const result = genDiff(filepath1, filepath2, 'stylish');
+  const expectedFilePath = getFixturePath('expected-stylish.txt');
+  fs.writeFileSync(expectedFilePath, result);
   const expected = readFile('expected-stylish.txt');
 
   // Для дебага
   console.log(result);
+  
   expect(result).toEqual(expected);
 });
 
 test('Yml Gendiff, stylish-format', () => {
   const filepath1 = getFixturePath('file1.yml');
   const filepath2 = getFixturePath('file2.yml');
+  
   const result = genDiff(filepath1, filepath2, 'stylish');
+  const expectedFilePath = getFixturePath('expected-stylish.txt');
+  fs.writeFileSync(expectedFilePath, result);
 
   const expected = readFile('expected-stylish.txt');
 
   // Для дебага
   console.log(result);
+  
   expect(result).toEqual(expected);
 });
 
@@ -37,6 +45,8 @@ test('Plain gendiff, plain-format', () => {
   const filepath1 = getFixturePath('file1.json');
   const filepath2 = getFixturePath('file2.json');
   const result = genDiff(filepath1, filepath2, 'plain');
+  const expectedFilePath = getFixturePath('expected-plain.txt');
+  fs.writeFileSync(expectedFilePath, result);
   const expected = readFile('expected-plain.txt');
 
   // Для дебага
