25359f7257efec32988ccd33367f1564aab41bb5
elvis178 <elvisss178@gmail.com>
2024-12-20 19:27:26 +0300

    update parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -5,7 +5,7 @@ import path from 'path';
 const parseFile = (filePath) => {
   const fileData = fs.readFileSync(filePath, 'utf-8');
 
-  const fileExtension = path.extname(filePath).slice(1).toLowerCase()
+  const fileExtension = path.extname(filePath).slice(1).toLowerCase();
 
   if (fileExtension === 'json') {
     return JSON.parse(fileData);
