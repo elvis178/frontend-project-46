217263df100d18913577cf783c756b568025a174
elvis178 <elvisss178@gmail.com>
2024-12-25 18:18:59 +0300

    update parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -1,20 +1,15 @@
-import fs from 'fs';
 import yaml from 'yaml';
-import path from 'path';
 
-const parseFile = (filePath) => {
-  const fileData = fs.readFileSync(filePath, 'utf-8');
-
-  const fileExtension = path.extname(filePath).slice(1).toLowerCase();
-
-  if (fileExtension === 'json') {
-    return JSON.parse(fileData);
+const parseFile = (format, data) => {
+  switch (format) {
+    case 'json':
+      return JSON.parse(data);
+    case 'yml':
+    case 'yaml':
+      return yaml.parse(data);
+    default:
+      throw new Error(`Unsupported file format: ${format}`);
   }
-
-  if (fileExtension === 'yml' || fileExtension === 'yaml') {
-    return yaml.parse(fileData);
-  }
-  throw new Error(`Unsupported file format: ${fileExtension}`);
 };
 
 export default parseFile;
