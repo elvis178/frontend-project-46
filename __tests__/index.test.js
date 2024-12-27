68d14177d0226b7b5f850db2cbd151cb4605c4c3
elvis178 <elvisss178@gmail.com>
2024-12-27 22:52:39 +0300

    update index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,25 +11,25 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 const testCases = [
-  ['file1.json', 'file2.json', 'stylish', 'expected-stylish.txt'],
-  ['file1.json', 'file2.yml', 'stylish', 'expected-stylish.txt'],
-  ['file1.yml', 'file2.yml', 'stylish', 'expected-stylish.txt'],
-  ['file1.yml', 'file2.json', 'stylish', 'expected-stylish.txt'],
-  ['file1.json', 'file2.json', 'json', 'expected-json.txt'],
-  ['file1.json', 'file2.yml', 'json', 'expected-json.txt'],
-  ['file1.yml', 'file2.yml', 'json', 'expected-json.txt'],
-  ['file1.yml', 'file2.json', 'json', 'expected-json.txt'],
-  ['file1.json', 'file2.json', 'plain', 'expected-plain.txt'],
-  ['file1.json', 'file2.yml', 'plain', 'expected-plain.txt'],
-  ['file1.yml', 'file2.yml', 'plain', 'expected-plain.txt'],
-  ['file1.yml', 'file2.json', 'plain', 'expected-plain.txt'],
+  ['file1.json', 'file2.json', 'expected-stylish.txt', 'stylish'],
+  ['file1.json', 'file2.yml', 'expected-stylish.txt', 'stylish'],
+  ['file1.yml', 'file2.yml', 'expected-stylish.txt', 'stylish',],
+  ['file1.yml', 'file2.json', 'expected-stylish.txt', 'stylish'],
+  ['file1.json', 'file2.json', 'expected-json.txt', 'json',],
+  ['file1.json', 'file2.yml', 'expected-json.txt', 'json',],
+  ['file1.yml', 'file2.yml', 'expected-json.txt', 'json',],
+  ['file1.yml', 'file2.json', 'expected-json.txt', 'json',],
+  ['file1.json', 'file2.json', 'expected-plain.txt', 'plain',],
+  ['file1.json', 'file2.yml', 'expected-plain.txt', 'plain',],
+  ['file1.yml', 'file2.yml', 'expected-plain.txt', 'plain',],
+  ['file1.yml', 'file2.json', 'expected-plain.txt', 'plain',],
 ];
 
 const defaultFormatter = 'stylish';
 
 test.each(testCases)(
   'gendiff between %s and %s files in %s format',
-  (file1, file2, format = defaultFormatter, expectedFile) => {
+  (file1, file2, expectedFile, format = defaultFormatter) => {
     const filepath1 = getFixturePath(file1);
     const filepath2 = getFixturePath(file2);
     const result = genDiff(filepath1, filepath2, format);
