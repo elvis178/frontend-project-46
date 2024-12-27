b1a6640f85813af534bb9e65e8bdbcaf6503d437
elvis178 <elvisss178@gmail.com>
2024-12-27 22:45:36 +0300

    update index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,24 +10,30 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-// Массив с тестовыми кейсами
 const testCases = [
   ['file1.json', 'file2.json', 'stylish', 'expected-stylish.txt'],
+  ['file1.json', 'file2.yml', 'stylish', 'expected-stylish.txt'],
   ['file1.yml', 'file2.yml', 'stylish', 'expected-stylish.txt'],
-  ['file1.json', 'file2.json', 'plain', 'expected-plain.txt'],
+  ['file1.yml', 'file2.json', 'stylish', 'expected-stylish.txt'],
   ['file1.json', 'file2.json', 'json', 'expected-json.txt'],
+  ['file1.json', 'file2.yml', 'json', 'expected-json.txt'],
+  ['file1.yml', 'file2.yml', 'json', 'expected-json.txt'],
+  ['file1.yml', 'file2.json', 'json', 'expected-json.txt'],
+  ['file1.json', 'file2.json', 'plain', 'expected-plain.txt'],
+  ['file1.json', 'file2.yml', 'plain', 'expected-plain.txt'],
+  ['file1.yml', 'file2.yml', 'plain', 'expected-plain.txt'],
+  ['file1.yml', 'file2.json', 'plain', 'expected-plain.txt'],
 ];
+
+const defaultFormatter = 'stylish';
 
 test.each(testCases)(
   'gendiff between %s and %s files in %s format',
-  (file1, file2, format, expectedFile) => {
+  (file1, file2, format = defaultFormatter, expectedFile) => {
     const filepath1 = getFixturePath(file1);
     const filepath2 = getFixturePath(file2);
     const result = genDiff(filepath1, filepath2, format);
     const expected = readFile(expectedFile);
-
-    // Для дебага
-    console.log(result);
 
     expect(result).toEqual(expected);
   },
--- src/buildDiff.js
+++ src/buildDiff.js
@@ -6,22 +6,18 @@ const buildDiff = (obj1, obj2) => {
   const sortedUnicKeys = _.sortBy(_.union(keys1, keys2));
 
   const resultObj = sortedUnicKeys.map((key) => {
-    // Если ключ отсутствует в obj1, значит он добавлен в obj2
     if (!Object.hasOwn(obj1, key)) {
       return { key, value: obj2[key], type: 'added' };
     }
 
-    // Если ключ отсутствует в obj2, значит он удален из obj2
     if (!Object.hasOwn(obj2, key)) {
       return { key, value: obj1[key], type: 'deleted' };
     }
 
-    // Если значения одинаковые, то ключ неизменен
     if (_.isEqual(obj1[key], obj2[key])) {
       return { key, value: obj1[key], type: 'unchanged' };
     }
 
-    // Если значения отличаются и оба являются простыми объектами, то вызываем рекурсивно buildDiff
     if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
       return { key, children: buildDiff(obj1[key], obj2[key]), type: 'nested' };
     }
